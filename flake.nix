cf58a155f416846fe84ce3f6ea98077cca1e4691
0xadk <0xadk@users.noreply.github.com>
2026-06-24 23:06:29 -0700

    build: cleanup flake
    - update flake
    - remove needless .envrc
    - remove flake-utils depend
    - filter out irrelevant files from crane package

--- flake.nix
+++ flake.nix
@@ -2,20 +2,29 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     crane.url = "github:ipetkov/crane";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import inputs.nixpkgs {inherit system;};
+  outputs = inputs: let
+    # @type (system: string) -> (pkgs: nixpkgs) -> { ${system} = any }
+    forAllSystems = f: builtins.mapAttrs f inputs.nixpkgs.legacyPackages;
+  in {
+    packages = forAllSystems (system: pkgs: let
       inherit (pkgs) lib;
       craneLib = inputs.crane.mkLib pkgs;
     in {
-      packages.default = craneLib.buildPackage {
-        # cleaning the source causes some tests to fail and I couldn't figure
-        # out the right files required to get them passing
-        src = ./.;
-        # src = craneLib.cleanCargoSource ./.;
-      };
+      default = let
+        unfilteredRoot = ./.;
+
+        src = lib.fileset.toSource {
+          root = unfilteredRoot;
+
+          fileset = lib.fileset.unions [
+            (craneLib.fileset.commonCargoSources unfilteredRoot)
+            ./tests
+          ];
+        };
+      in
+        craneLib.buildPackage {inherit src;};
     });
+  };
 }
